296a95a314927d51577575f68ee241be173c682d
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-24 12:26:28 +0100

    start work

---
+++ client/src/Components/Routes/DashBoardRoutes.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashBoardRoutes = () => {
+  return (
+    <div>DashBoardRoutes</div>
+  )
+}
+
+export default DashBoardRoutes
---
+++ client/src/Components/Routes/HomeRoutes.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeRoutes = () => {
+  return (
+    <div>HomeRoutes</div>
+  )
+}
+
+export default HomeRoutes
