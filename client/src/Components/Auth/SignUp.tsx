f75502d6ab89c8c7963b382cf3351df4c8d99c19
Favour-Yusuf <favouryusuf45@gmail.com>
2023-03-24 18:02:55 +0100

    sign up responsive

--- client/src/Components/Auth/SignUp.tsx
+++ client/src/Components/Auth/SignUp.tsx
@@ -107,25 +107,32 @@ export default SignUp;
 
 const RightImg = styled.img`
   width: 500px;
-  color: #039ee6;
 `;
 
 const Right = styled.div`
   width: 50%;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
+
+// const Body = styled.div``;
 
 const Already = styled.div`
   font-size: 13px;
   cursor: pointer;
-  color: #039ee6;
+  color: #6c63ff;
   margin-top: 15px;
   text-align: center;
+  @media screen and (max-width: 425px) {
+    font-size: 10px;
+  }
 `;
 
 const Button = styled.button`
   width: 105%;
   height: 40px;
-  background: #039ee6;
+  background: #6c63ff;
   color: white;
   border: none;
   border-radius: 7px;
@@ -136,29 +143,49 @@ const Button = styled.button`
   }
 `;
 
-const Input = styled.input`
+const Input = styled.input` // <{ props: string }>
   width: 100%;
   height: 40px;
   border: none;
-  box-shadow: 0 0 2px #039ee6;
+  box-shadow: 0 0 2px #6c63ff;
   margin-bottom: 20px;
   border-radius: 7px;
   padding-left: 10px;
+  @media screen and (max-width: 425px) {
+    box-shadow: none;
+    border-bottom: 1px solid #6c63ff;
+  }
+  @media screen and (max-width: 768px) {
+    height: 35px;
+  }
 `;
 
 const Form = styled.form`
   width: 270px;
-  height: 450px;
-  box-shadow: 0 0 3px #039ee6;
+  height: 400px;
+  box-shadow: 0 0 3px #6c63ff;
   border-radius: 10px 0 10px 0;
   padding: 30px;
-  padding-bottom: 30px;
   padding-right: 40px;
-  margin-top: 40px;
-
-  p {
-    color: red;
-    font-size: 12px;
+  @media screen and (max-width: 425px) {
+    width: 310px;
+    padding: 20px;
+    padding-right: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    box-shadow: none;
+  }
+  @media screen and (max-width: 320px) {
+    width: 310px;
+    padding: 10px;
+    padding-right: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  @media screen and (max-width: 768px) {
+    height: 400px;
   }
 `;
 
@@ -168,6 +195,12 @@ const Left = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  @media screen and (max-width: 768px) {
+    width: 85%;
+  }
+  @media screen and (max-width: 425px) {
+    width: 95%;
+  }
 `;
 
 const Hold = styled.div`
@@ -176,6 +209,9 @@ const Hold = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @media screen and (max-width: 768px) {
+    justify-content: center;
+  }
 `;
 
 const Body = styled.div`
