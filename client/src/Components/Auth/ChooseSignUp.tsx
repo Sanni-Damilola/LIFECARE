2bfa3ad738fbcaa38ec4cebbf1952c0d5815c738
Favour-Yusuf <favouryusuf45@gmail.com>
2023-03-27 15:23:14 +0100

    update

--- client/src/Components/Auth/ChooseSignUp.tsx
+++ client/src/Components/Auth/ChooseSignUp.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import pic from "../Images/medical-care.svg";
 
 const ChooseSignUp = () => {
   return (
@@ -23,3 +24,76 @@ const ChooseSignUp = () => {
 };
 
 export default ChooseSignUp;
+
+const Input = styled.div`
+  height: 30px;
+  width: 250px;
+  border: 1px light gray;
+  border-radius: 5px;
+  font-weight: 500;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  margin-bottom: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #7bb136;
+  cursor: pointer;
+`;
+const Box = styled.div`
+  height: 300px;
+  width: 300px;
+  box-shadow: rgba(209, 101, 101, 0.2) 0px 2px 8px 0px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid #a7fd3784;
+
+  p {
+    font-size: 20px;
+    color: black;
+    margin-bottom: 20px;
+  }
+`;
+const Right = styled.div`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Img = styled.img`
+  height: 300px;
+`;
+const Left = styled.div`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Head = styled.div`
+  height: 30vh;
+  width: 100%;
+  background-color: #9fdf4c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  font-weight: 500;
+  color: #ffffff;
+  border-bottom-left-radius: 30%;
+`;
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
--- client/src/Components/Routes/HomeRoutes.tsx
+++ client/src/Components/Routes/HomeRoutes.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
+import ChooseSignUp from "../Auth/ChooseSignUp";
 import Signin from "../Auth/Signin";
 import SignUp from "../Auth/SignUp";
+import ComingSoon from "../BloodDonation/ComingSoon";
 import LandingPage from "../Home/LandingPage";
 
 const HomeRoutes = () => {
@@ -12,7 +14,7 @@ const HomeRoutes = () => {
     },
     {
       path: "/signup",
-      element: <SignUp />,
+      element: <ChooseSignUp />,
     },
 
     {
@@ -23,6 +25,10 @@ const HomeRoutes = () => {
     {
       path: "/dashboard",
       element: <Signin />,
+    },
+    {
+      path: "/donateblood",
+      element: <ComingSoon />,
     },
   ]);
   return element;
