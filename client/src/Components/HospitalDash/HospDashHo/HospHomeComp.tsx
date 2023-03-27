2519312ed9d97f6c03de3c91581b91dff4812b62
abrahamk29@gmail.com <abrahamk29@gmail.com>
2023-03-27 12:26:40 +0100

    HospDashboard

---
+++ client/src/Components/HospitalDash/HospDashHo/HospHomeComp.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import HospDashHome1 from "./HospDashHome1";
+import HospDashHome2 from "./HospDashHome2";
+
+
+
+const HospHomeComp = () =>{
+
+    return (
+        <>
+
+           <HospDashHome1 />
+           <HospDashHome2 />
+
+        </>
+    )
+}
+
+export default HospHomeComp;
